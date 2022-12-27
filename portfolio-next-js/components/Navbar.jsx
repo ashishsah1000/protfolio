7c1ab95e3ae4ed714f49bafade43ca0750579e11
Ashish sah <ashishsah1000@gmail.com>
2022-12-27 06:44:29 +0000

    aded next js and tailwind css for the setup

---
+++ portfolio-next-js/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full h-20 bg-red-400 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <h1>Hellow</h1>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
