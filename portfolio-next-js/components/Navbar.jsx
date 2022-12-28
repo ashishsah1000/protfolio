1e44960b5ec254e3f3060369a91c8b197a97e435
Ashish sah <ashishsah1000@gmail.com>
2022-12-28 06:04:30 +0000

    Added navbar side nav

--- portfolio-next-js/components/Navbar.jsx
+++ portfolio-next-js/components/Navbar.jsx
@@ -1,11 +1,69 @@
 import Image from "next/image";
 import React from "react";
+import Link from "next/link";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const myLoader = ({ src }) => {
+    return "https://upload.wikimedia.org/wikipedia/commons/6/6f/HP_logo_630x630.png";
+  };
   return (
-    <div className="fixed w-full h-20 bg-red-400 shadow-xl z-[100]">
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-        <h1>Hellow</h1>
+        <Image
+          loader={myLoader}
+          src="https://upload.wikimedia.org/wikipedia/commons/6/6f/HP_logo_630x630.png"
+          height="50"
+          width="80"
+          alt="/"
+        />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Projects
+              </li>
+            </Link>
+          </ul>
+          <div className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+      <div className="fixed left-0 top-0 w-full h-screen bg-black/50">
+        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+          <div>
+            <div className="flex w-full items-center justify-between">
+              <Image
+                loader={myLoader}
+                src="https://upload.wikimedia.org/wikipedia/commons/6/6f/HP_logo_630x630.png"
+                height="50"
+                width="80"
+                alt="/"
+              />
+              <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+                <AiOutlineClose />
+              </div>
+            </div>
+            <div className="border-b border-gray-300 my-4">
+              <p className="w-[85%] md:w-[90%] py-4">
+                Let's build something legendary together
+              </p>
+            </div>
+          </div>
+          <div>
+            
+          </div>
+        </div>
       </div>
     </div>
   );
